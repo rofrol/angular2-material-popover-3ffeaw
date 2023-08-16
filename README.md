98865d1cb6fd124f53c45ad8845abc603adcfe65
rofrol <rofrol@gmail.com>
2023-08-16 17:43:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular2-material-popover-3ffeaw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular2-material-popover-3ffeaw)
---
+++ app/popover-example.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'popover-example',
+  templateUrl: 'popover-example.html',
+  styleUrls: ['popover-example.css'],
+})
+export class PopOverExample {}
